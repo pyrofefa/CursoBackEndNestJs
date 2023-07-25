70fb6714702f0d1b37a69585c374188a9b126fdf
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-07-25 14:33:08 -0700

    pipes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -6,25 +6,19 @@ import {
   Param,
   Post,
   Put,
-  Query,
   HttpStatus,
   HttpCode,
-  Res,
+  //ParseIntPipe,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { ProductsService } from '../services/products.service';
+import { ParseIntPipe } from '../common/parse-int/parse-int.pipe';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productService: ProductsService) {}
 
   @Get()
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    //return `Product limit => ${limit} offset => ${offset} brand => ${brand}`;
+  getProducts() {
     return this.productService.findAll();
   }
 
@@ -32,44 +26,24 @@ export class ProductsController {
   getProductFilter() {
     return `Yo soy un filter`;
   }
-
-  /*@Get(':id')
-  getProduct(@Res() response: Response, @Param('id') id: string) {
-    response.status(200).send({
-      message: `Product ${id}`,
-    });
-  }*/
-
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Param('id') id: string) {
-    /*return {
-      message: `Product ${id}`,
-    };*/
-    return this.productService.findOne(+id);
+  getProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.findOne(id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    /*return {
-      message: 'Accion de crear',
-      payload,
-    };*/
     return this.productService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    /*return {
-      id,
-      payload,
-    };*/
     return this.productService.update(+id, payload);
   }
 
   @Delete(':id')
   destroy(@Param('id') id: number) {
-    //return id;
     return this.productService.remove(+id);
   }
 }
