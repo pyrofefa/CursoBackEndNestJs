6a55f144cb8c90aed8a23a81eafdd99982884a01
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-07-25 11:31:48 -0700

    Services

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -12,16 +12,20 @@ import {
   Res,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { ProductsService } from '../services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `Product limit => ${limit} offset => ${offset} brand => ${brand}`;
+    //return `Product limit => ${limit} offset => ${offset} brand => ${brand}`;
+    return this.productService.findAll();
   }
 
   @Get('filter')
@@ -38,33 +42,34 @@ export class ProductsController {
 
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Res() response: Response, @Param('id') id: string) {
-    response.status(200).send({
+  getProduct(@Param('id') id: string) {
+    /*return {
       message: `Product ${id}`,
-    });
-    return {
-      message: `Product ${id}`,
-    };
+    };*/
+    return this.productService.findOne(+id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
+    /*return {
       message: 'Accion de crear',
       payload,
-    };
+    };*/
+    return this.productService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
+    /*return {
       id,
       payload,
-    };
+    };*/
+    return this.productService.update(+id, payload);
   }
 
   @Delete(':id')
   destroy(@Param('id') id: number) {
-    return id;
+    //return id;
+    return this.productService.remove(+id);
   }
 }
