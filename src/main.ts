77aa9968aa9097052561d3e7e56e34b61c2bd62e
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-07-27 12:11:08 -0700

    Evitar parametros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Ignorar datos que no esten en los DTO
+      forbidNonWhitelisted: true, // Lanzar error si existen datos prohibidos
+      disableErrorMessages: false, // Desabilitar mensajes de error (producción)
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
