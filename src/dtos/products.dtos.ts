e68a385bc58189b16b3f3fc9cd9692ccba617221
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-07-27 11:15:45 -0700

    Class validator and mapped-types

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,34 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsNotEmpty()
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
