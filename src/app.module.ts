3f4359dddf9963a6160bb705f1e5bf09e06ac2bb
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-08-15 13:38:04 -0700

    Validando esquemas envs con joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { lastValueFrom } from 'rxjs';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
@@ -20,6 +22,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
