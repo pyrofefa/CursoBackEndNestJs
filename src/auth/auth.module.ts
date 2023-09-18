2e0912fe4bbb125cf388a266968f0c23eb3716be
estebanCesave <francisco.flores@cesaveson.com>
2023-09-18 16:39:48 -0700

    Autentificacion con passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './services/auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
-@Module({})
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
