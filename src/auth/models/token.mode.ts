80b28179832d84d10d08a9b2e0579c1a5f227e8c
estebanCesave <francisco.flores@cesaveson.com>
2023-09-18 17:09:19 -0700

    Conectando Passport con JWT

---
+++ src/auth/models/token.mode.ts
@@ -0,0 +1,4 @@
+export interface PlayloadToken {
+  role: string;
+  sub: number;
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/services/users.service';
+import { PlayloadToken } from '../models/token.mode';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
@@ -13,5 +20,13 @@ export class AuthService {
       return user;
     }
     return null;
+  }
+
+  generateJWT(user: User) {
+    const playload: PlayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(playload),
+      user,
+    };
   }
 }
